ce4888c25a0031bf5df20065c55e4bfe1009e9c1
Sunil Kamkar Sheshagiri <41332941+sunilks12@users.noreply.github.com>
2023-01-24 22:51:34 -0600

    Update wave array.py

--- array/wave array.py
+++ array/wave array.py
@@ -6,6 +6,25 @@ class Solution:
         # code here
         for i in range(0, n-1, 2):
             a[i], a[i + 1] = a[i + 1], a[i]
+
             
+
+
+
+class Solution:
+    def convertToWave(self, n : int, a : List[int]) -> None:
+        # code here
+        if(n%2!=0):
+            i=0
+            while(i<n-2):
+                a[i],a[i+1]=a[i+1],a[i]
+                i+=2
+        if(n%2==0):
+            i=0
+            while(i<n-1):
+                a[i],a[i+1]=a[i+1],a[i]
+                i+=2
         
+
+
 
